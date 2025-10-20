1075f001262279d6296461600fb57463b3ad72d3
Abhay-0103 <ab0321054@gmail.com>
2025-10-20 20:44:50 +0530

    Completed Landing page

--- frontend/src/pages/LandingPage/LandindPage.jsx
+++ frontend/src/pages/LandingPage/LandindPage.jsx
@@ -5,14 +5,18 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import Analytics from './components/Analytics'
+import Footer from './components/Footer'
 
 const LandindPage = () => {
   return (
-    <div className='min-h-screen mb-[100vh]'>
+    <div className='min-h-screen'>
       <Header />
       <Hero />
       <Features />
       <Analytics />
+      <Footer />
+
+
     </div>
   )
 }
