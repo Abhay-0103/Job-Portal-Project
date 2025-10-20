b889100842a66421c82c5df824363eeb9dda86f1
Abhay-0103 <ab0321054@gmail.com>
2025-10-20 20:55:36 +0530

    buliding Login Form Page

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion';
+import {
+  Mail,
+  Lock,
+  Eye,
+  EyeOff,
+  Loader,
+  AlertCircle,
+  CheckCircle
+} from 'lucide-react';
 
 const Login = () => {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    rememnerMe: false
+  });
+
+  const [formState, setFormState] = useState({
+    loading: false,
+    error: {},
+    showPassword: false,
+    success: false
+  });
+
+  // Validation Functions
+  const validateEmail = (email) => {
+
+  };
+
+  const validatePassword = (password) => {
+
+  };
+
+  // Handle Input Changes
+  const handleInputChange = (e) => {
+
+  };
+
+  const validateForm = () => {
+
+  };
+
+  const handleSubmit = async (e) => {
+    
+  };
+
   return (
-    <div>Login</div>
+    
   )
 }
 
