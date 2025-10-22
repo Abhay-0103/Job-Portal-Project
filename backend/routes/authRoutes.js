dad9e48d51bdd26a9687b1d579cf4de6430ae208
Abhay-0103 <ab0321054@gmail.com>
2025-10-23 00:26:42 +0530

    Upload images

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,5 +8,15 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+    }`;
+    res.status(200).json({ imageUrl });
+});
+
 
 module.exports = router;
