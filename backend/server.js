5bf4d3904d2018cc113927f46066c29511dc445d
Abhay-0103 <ab0321054@gmail.com>
2025-10-24 15:29:44 +0530

    Add savedJobsRoutes to server.js for handling saved jobs API

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const connectDB = require("./config/db");
 
 // local imports
+const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
+const savedJobsRoutes = require("./routes/savedJobsRoutes");
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use("/api/applications", applicationRoutes);
+app.use("/api/save-jobs", savedJobsRoutes);
 
 // serve uploads folder 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {}));
