08f4da32721ec29dfc388765c1a6c2972774d5c0
Abhay-0103 <ab0321054@gmail.com>
2025-10-24 19:58:27 +0530

    Fix import case for protect middleware in savedJobsRoutes

--- backend/routes/savedJobsRoutes.js
+++ backend/routes/savedJobsRoutes.js
@@ -3,15 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 // Local imports
-const { Protect } = require('../middlewares/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 const {
     saveJob,
     unsaveJob,
     getMySavedJobs,
 } = require('../controllers/savedJobController');
 
-router.post('/:jobId', Protect, saveJob);
-router.delete('/:jobId', Protect, unsaveJob);
-router.get('/my', Protect, getMySavedJobs);
+router.post('/:jobId', protect, saveJob);
+router.delete('/:jobId',protect, unsaveJob);
+router.get('/my', protect, getMySavedJobs);
 
 module.exports = router;
