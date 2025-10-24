4bdd8224c840e9de7e493ee84b05c16b8e183b42
Abhay-0103 <ab0321054@gmail.com>
2025-10-24 13:19:58 +0530

    code improvement

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -2,8 +2,8 @@ const multer = require('multer');
 
 // Configure storage for uploaded files
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Specify the destination directory
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // Specify the destination directory
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`) // Specify the file name
@@ -18,7 +18,7 @@ const fileFilter = (req, file, cb) => {
     }
     else {
         cb(new Error('Invalid file type. Only JPEG, PNG, JPG, and PDF are allowed.'), false);
-    }   
+    }
 };
 
 // Initialize multer with the defined storage and file filter
