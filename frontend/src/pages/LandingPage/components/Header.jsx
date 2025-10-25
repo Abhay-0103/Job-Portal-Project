3bcd73a95ec11fc39faf9da0d2dfac6da028677f
Abhay-0103 <ab0321054@gmail.com>
2025-10-25 17:04:36 +0530

    Refactor Header component to utilize useAuth for user authentication

--- frontend/src/pages/LandingPage/components/Header.jsx
+++ frontend/src/pages/LandingPage/components/Header.jsx
@@ -1,11 +1,14 @@
+// global Imports
 import React from "react";
 import { motion } from "framer-motion";
 import { Briefcase } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
+// Local Imports
+import { useAuth } from "../../../context/AuthContext";
+
 const Header = () => {
-  const isAuthenticated = true;
-  const user = { FullName: "Alex", role: "employer" };
+  const {user, isAuthenticated} = useAuth();
   const Navigate = useNavigate();
 
   return (
