d11531ffc1cf9d242ee234083a869f57acc3467e
Abhay-0103 <ab0321054@gmail.com>
2025-10-25 21:53:01 +0530

    Refactor EmployerDashboard component to include data fetching and loading state management

--- frontend/src/pages/Employer/EmployerDashboard.jsx
+++ frontend/src/pages/Employer/EmployerDashboard.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+// global imports
+import React, { useEffect, useState } from "react";
+import {
+  Plus,
+  Briefcase,
+  Users,
+  Building2,
+  TrendingUp,
+  CheckCircle2,
+} from "lucide-react";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
+
+// local imports
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import DashboardLayout from "../../components/layout/DashboardLayout";
 
 const EmployerDashboard = () => {
-  return (
-    <div>EmployerDashboard</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default EmployerDashboard
+  const [dashboardData, setDashboardData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getDashboardOverView = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axiosInstance.get(API_PATHS.DASHBOARD.OVERVIEW);
+      if (response.status === 200) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDashboardOverView();
+    return () => {};
+  }, []);
+
+  return <DashboardLayout activeMenu="employer-dashboard"></DashboardLayout>;
+};
+
+export default EmployerDashboard;
