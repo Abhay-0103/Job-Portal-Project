ac2057cb87eb62e5cb3e147edd70fd640bca86d5
Abhay-0103 <ab0321054@gmail.com>
2025-10-29 22:48:57 +0530

    Fix casing for job seeker field in saved job controller

--- backend/controllers/savedJobController.js
+++ backend/controllers/savedJobController.js
@@ -3,14 +3,14 @@ const SavedJob = require('../models/SavedJob');
 // Save a job
 exports.saveJob = async (req, res) => {
     try {
-        const exists = await SavedJob.findOne({ job: req.params.jobId, jobseeker: req.user._id });
+        const exists = await SavedJob.findOne({ job: req.params.jobId, jobSeeker: req.user._id });
         if (exists) {
             return res.status(400).json({ message: "Job already saved" });
         }
 
         const saved = await SavedJob.create({
             job: req.params.jobId,
-            jobseeker: req.user._id
+            jobSeeker: req.user._id
         });
         res.status(201).json(saved);
     } catch (err) {
@@ -21,7 +21,7 @@ exports.saveJob = async (req, res) => {
 // Unsave a job
 exports.unsaveJob = async (req, res) => {
      try {
-        await SavedJob.findOneAndDelete({ job: req.params.jobId, jobseeker: req.user._id });
+        await SavedJob.findOneAndDelete({ job: req.params.jobId, jobSeeker: req.user._id });
         res.json({ message: "Job removed from saved list" });
     } catch (err) {
         res.status(500).json({ message: "Failed to remove saved job", error: err.message });
@@ -31,7 +31,7 @@ exports.unsaveJob = async (req, res) => {
 // Get saved jobs for current user
 exports.getMySavedJobs = async (req, res) => {
      try {
-        const savedJobs = await SavedJob.find({ jobseeker: req.user._id })
+        const savedJobs = await SavedJob.find({ jobSeeker: req.user._id })
         .populate({
             path: 'job',
             populate: {
