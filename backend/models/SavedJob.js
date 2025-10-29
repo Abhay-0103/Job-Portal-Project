0aeb48157abb70904ed54d3f2d9501942c0475ae
Abhay-0103 <ab0321054@gmail.com>
2025-10-29 22:49:12 +0530

    Fix casing for job seeker field in SavedJob schema

--- backend/models/SavedJob.js
+++ backend/models/SavedJob.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const savedJobSchema = new mongoose.Schema(
     {
-        jobseeker: {
+        jobSeeker: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User',
             required: true,
